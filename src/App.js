0c599990dfa56a37332b0bc775e90b6f7fda9854
kiu_melikidze.archili <melikidze.archili@kiu.edu.ge>
2023-02-24 21:03:22 +0400

    modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/Loader.js 	new file:   src/imgs/Spinner-1s-200px.svg 	new file:   src/imgs/background.jpg 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	new file:   src/styles.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Loader } from "./Loader";
+import "./styles.css";
 
 function App() {
+  const [riddles, setRiddles] = useState('');
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+
+  const getRiddle = async () => {
+    setLoading(true);
+    await fetch("https://riddles-api.vercel.app/random")
+      .then(res => res.json())
+      .then(data => setRiddles(data));  
+
+    setShowAnswer(false);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getRiddle();
+  }, [])
+
+  const answerToggler = () => setShowAnswer(true);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Riddles">
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <div className="riddle">
+        {loading ? (
+          <Loader />
+        ) : (<div className="data">
+          <p className="question">Riddle: {riddles.riddle}</p>
+          <p className="answer">Answer:&nbsp;
+            {!showAnswer &&
+              <button className="show" onClick={answerToggler}>{riddles.answer}</button>
+            }
+            {showAnswer && <span>{riddles.answer}</span>}</p>
+        </div>)}
+        <div className="btnContainer">
+          <button onClick={getRiddle} className="btn">
+            ?
+          </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
